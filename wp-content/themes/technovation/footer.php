c6d60d0013fd2c45d39791e8c5cf072c43be41b8
Alli Colyer <allicolyer@gmail.com>
2019-10-17 14:06:21 -0700

    update copyright in footer

--- wp-content/themes/technovation/footer.php
+++ wp-content/themes/technovation/footer.php
@@ -14,7 +14,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php get_sidebar(); ?>
 		<div class="site-info">
-			&copy; <?php echo date('Y'); ?> <a href = "http://iridescentlearning.org/" target = "_blank">Iridescent</a>. All Rights Reserved. <?php bloginfo( 'name' ); ?> is a program of <a href = "http://iridescentlearning.org/" target = "_blank">Iridescent</a>, a 501c3 nonprofit organization in the United States.
+			&copy; <?php echo date('Y'); ?> Technovation. All Rights Reserved. Technovation is a 501c3 nonprofit organization in the United States.
 		</div>
 	</footer>
 </div>
